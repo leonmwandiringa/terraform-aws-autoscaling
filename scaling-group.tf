46fb35eb5989a5070ae65d14e119bedad67125e1
leonvodacom <leon.mwandiringa@vcontractor.co.za>
2024-04-07 17:37:18 +0200

    vpc zone identifier

--- scaling-group.tf
+++ scaling-group.tf
@@ -1,12 +1,12 @@
 resource "aws_autoscaling_group" "default" {
-  availability_zones = var.availability_zones
+  availability_zones = length(var.availability_zones) > 0 ? var.availability_zones : null
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
   health_check_grace_period = var.health_check_grace_period
   health_check_type = var.health_check_type
   protect_from_scale_in     = var.protect_from_scale_in
-  vpc_zone_identifier = var.vpc_zone_identifier
+  vpc_zone_identifier = length(var.vpc_zone_identifier) > 0 ? var.vpc_zone_identifier : null
 
   dynamic "tag" {
     for_each = var.asg_tags
